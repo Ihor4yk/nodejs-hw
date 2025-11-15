d6607a13ed033909cff689bb3f930cd79bd90963
Ihor4yk <igor4yk.lyashen4yk@gmail.com>
2025-11-15 22:48:34 +0200

    Add validation, pagination and filters

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { model, Schema } from "mongoose";
+import { TAGS } from "../constants/tags.js";
 
 const noteSchema = new Schema(
   {
@@ -6,18 +7,7 @@ const noteSchema = new Schema(
     content: { type: String, trim: true, default: "" },
     tag: {
       type: String,
-      enum: [
-        "Work",
-        "Personal",
-        "Meeting",
-        "Shopping",
-        "Ideas",
-        "Travel",
-        "Finance",
-        "Health",
-        "Important",
-        "Todo",
-      ],
+      enum: TAGS,
       default: "Todo",
     },
   },
@@ -25,5 +15,7 @@ const noteSchema = new Schema(
     timestamps: true,
   },
 );
+
+noteSchema.index({ title: "text", content: "text" });
 
 export const Note = model("Note", noteSchema);
